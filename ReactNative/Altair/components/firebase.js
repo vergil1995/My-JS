babc3c3aa82f0e6f48d5f773ddba95e4106c5c72
VERGIL-PC\Altair <narala1610@gmail.com>
2018-03-15 22:52:05 +0700

    Commit

---
+++ ReactNative/Altair/components/firebase.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import {
+    Platform,
+    StyleSheet,
+    Text,
+    View,
+    Image, AppRegistry, FlatList, ImageBackground,
+    Alert, TouchableHighlight, TouchableOpacity
+} from 'react-native';
+//import firebase from 'react-native-firebase';
+
+//export = public, Component = tag in HTML
+export default class BasicFireBase extends Component {
+    constructor(props){
+        // super goi contructor cua lop cha-Component
+           super(props); 
+        
+    }
+    render(){
+        return(
+            <View style={styles.styleview}>
+                <Text style={{color: red}}></Text>
+            </View> 
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    styleview: {
+        flex: 1, 
+        justifyContent: 'center', 
+        alignItems: 'center',
+        backgroundColor: '#00CC99',
+        marginTop: Platform.OS === 'ios' ? 34 : 0
+    },
+    style2: {
+        width: 100, 
+        height: 200, 
+        backgroundColor: 'blue'
+    },
+    style3: {
+        width: 100, 
+        height: 200, 
+        backgroundColor: 'red'
+    }
+});
