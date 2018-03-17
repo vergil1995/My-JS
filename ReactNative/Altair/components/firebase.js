b3b81f1092604217527f40de3a4f99415b2dda4c
VERGIL-PC\Altair <narala1610@gmail.com>
2018-03-17 17:02:31 +0700

    Login Anonymous

--- ReactNative/Altair/components/firebase.js
+++ ReactNative/Altair/components/firebase.js
@@ -9,18 +9,49 @@ import {
 } from 'react-native';
 //import * as firebase from 'firebase';
 import {firebaseApp} from '../config/firebase';
+//import firebase from 'react-native-firebase';
+import Button from 'react-native-button';
+import { error } from 'util';
 
 //export = public, Component = tag in HTML
 export default class BasicFireBase extends Component {
     constructor(props){
         // super goi contructor cua lop cha-Component
            super(props); 
+           this.state = {
+               isAuthenticated: false
+           };
         
+    }
+    onAnonymous = () => {
+        firebaseApp.auth().signInAnonymously()
+        .then(() => {
+            console.log('Login OK');
+            this.setState({
+                // render dc goi lai khi state thay doi
+                isAuthenticated: true
+            });
+        })
+        .catch((error)=>{
+            console.log(`Login Failed. Error = ${error}`);
+        })
     }
     render(){
         return(
             <View style={styles.styleview}>
-                <Text style={{color: 'red'}}>FireBase is OK</Text>
+                <Button containerStyle={{
+                    padding: 10,
+                    borderRadius: 4,
+                    backgroundColor: 'white'
+
+                }}
+                        style = {{fontSize: 18, color: 'blue'}}
+                        onPress = {this.onAnonymous}
+                >
+                Login Anonymous
+                </Button>
+                <Text style={{margin: 20, fontSize: 15}}
+                >{this.state.isAuthenticated == true ? "Login in anonymous ": ""}</Text>
             </View> 
         );
     }
